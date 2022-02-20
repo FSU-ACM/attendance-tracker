5a7db7f60b00eb678d193ec5c8271aab24e2dbd0
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2022-02-19 22:22:48 -0600

    documentation + various updates
    Only write header on new file.
    Changed formatting of exit statements.
    Removed first name trailing whitespace.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import csv, getpass
 
 if __name__ == '__main__':
 	correct = 'n'
-	loop = True
 
+	# Get filename details
 	while correct == 'n' or correct == 'N':
 		date = input('Event date [YYYY-MM-DD]: ')
 		event = input('Event name: ')
@@ -12,29 +12,47 @@ if __name__ == '__main__':
 		print('\n')
 
 	filename = date + '_' + event + '.csv'
-	with open(filename, "w") as f:
-		
+	
+	# Get length of file if it exists.
+	length = 0
+	try:
+		with open(filename, 'r') as f:
+			reader = csv.reader(f, delimiter=',')
+			length = len(list(reader))
+	except:
+		pass
+	
+	# Write routine
+	with open(filename, 'a') as f:
 
 		writer = csv.writer(
 			f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-		writer.writerow(['id', 'lastname', 'firstname'])
+		
+		# Only write header to a new file
+		if length == 0:
+			writer.writerow(['id', 'lastname', 'firstname'])
 
 		swipe = None
 		count = 0
+
+		# Accept user swipes and write to specified file.
 		while(swipe != 'quit'):
 			
 			swipe = getpass.getpass(prompt='Please swipe your FSU Card:')
 			
+			# Valid read
 			if len(swipe) > 2 and swipe[1] == 'B':
 				id = swipe[10:18]
 				name = swipe.split('^')
 				lastname = name[1].split('/')[0]
-				firstname = name[1].split('/')[1]
+				firstname = name[1].split('/')[1].rstrip()
 				
 				count += 1
 				writer.writerow([id, lastname, firstname])
 				print('Thanks for checking in ' + firstname+ '!\n')				
+			# Invalid read
 			elif swipe != 'quit':
 				print('READ ERROR : PLEASE RESWIPE\n')
-
-		print('\nChecked in:', count)
+			# Display checkin data on exit
+			else:
+				print('\n*****Quitting Tracker*****\nCHECKED IN:', count, '\n')		
